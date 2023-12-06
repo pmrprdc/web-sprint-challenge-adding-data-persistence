e08f9aedcf81471854bd012c7bc5c4e9d7a898de
Pamir Perdeci <pamirperdeci@gmail.com>
2023-12-06 18:40:25 -0500

    all projects and resources end points meet spec

--- api/resource/router.js
+++ api/resource/router.js
@@ -9,9 +9,15 @@ router.post('/', async (req, res) => {
         const newResource = await dataAccess.addResource(req.body);
         res.status(201).json(newResource);
     } catch (error) {
-        // Error handling
+        // Check for a unique constraint violation
+        if (error.code === 'SQLITE_CONSTRAINT' || error.code === '23505') { // '23505' is the code for unique violation in PostgreSQL
+            return res.status(400).json({ message: 'A resource with this name already exists' });
+        }
+        // General error handling
+        res.status(500).json({ message: 'Error adding the resource' });
     }
 });
+
 
 // [GET] /api/resources
 router.get('/', async (req, res) => {
