3c3f9ec00a8f79b1a54ab6ef9080d12ac86e83b8
Pamir Perdeci <pamirperdeci@gmail.com>
2023-12-05 20:49:28 -0500

    created server and starts on run of index

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
-// build your server here and require it from index.js
+const express = require('express');
+const server = express();
+
+// Middleware
+server.use(express.json()); // for parsing application/json
+
+// Basic Route
+server.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Export the server
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// start your server here
+const server = require('./api/server'); // Import the server
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
