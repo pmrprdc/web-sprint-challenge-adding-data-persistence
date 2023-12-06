c16e814ad98746e7680aff75cf665c4b2d4fdadf
Pamir Perdeci <pamirperdeci@gmail.com>
2023-12-06 18:30:08 -0500

    projects post and get api endpoints passing spec, could use some middleware and knex global error handling later

--- api/project/model.js
+++ api/project/model.js
@@ -3,8 +3,17 @@
 const db = require('../../data/dbConfig');
 
 function addProject(projectData) {
+    // Convert boolean to integer for database storage
     projectData.project_completed = projectData.project_completed ? 1 : 0;
-    return db('projects').insert(projectData).returning('*');
+
+    return db('projects')
+        .insert(projectData)
+        .returning('*') // Return all fields of the newly added project
+        .then(projects => projects[0]) // Assuming that the database returns an array
+        .then(project => ({
+            ...project,
+            project_completed: project.project_completed === 1 // Convert back to boolean
+        }));
 }
 
 function getAllProjects() {
--- api/project/router.js
+++ api/project/router.js
@@ -6,13 +6,21 @@ const dataAccess = require('./model'); // Adjust the path as needed
 
 // [POST] /api/projects
 router.post('/', async (req, res) => {
+    // Check if project_name is provided
+    if (!req.body.project_name) {
+        // If project_name is missing, respond with a 400 Bad Request status
+        return res.status(400).json({ message: 'Project name is required' });
+    }
+
     try {
         const newProject = await dataAccess.addProject(req.body);
         res.status(201).json(newProject);
     } catch (error) {
-        // Error handling
+        // Error handling, e.g., respond with a 500 Internal Server Error status
+        res.status(500).json({ message: 'Error adding the project' });
     }
 });
+
 
 // [GET] /api/projects
 router.get('/', async (req, res) => {
