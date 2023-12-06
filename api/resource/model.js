484a02e5516011f89d9543598145c690cc4b98ce
Pamir Perdeci <pamirperdeci@gmail.com>
2023-12-06 18:35:12 -0500

    resources post api returns newly created post

--- api/resource/model.js
+++ api/resource/model.js
@@ -4,7 +4,10 @@
 const db = require('../../data/dbConfig'); // Adjust the path to your Knex configuration
 
 function addResource(resourceData) {
-    return db('resources').insert(resourceData).returning('*');
+    return db('resources')
+        .insert(resourceData)
+        .returning('*') // Return all fields of the newly added resource
+        .then(resources => resources[0]); // Assuming the database returns an array
 }
 
 function getAllResources() {
